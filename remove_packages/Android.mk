f6be44b644d2ca6225e76df02fbddf983e0b82c2
tedomi2705 <hongducthbk123@gmail.com>
2022-05-13 13:18:38 +0700

    tulip: remove_packages: Nuke more and less packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,13 +4,56 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService BetterBug Camera2 CarrierMetrics CarrierSetup CarrierWifi ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService DeviceIntelligenceNetworkPrebuilt DevicePolicyPrebuilt DiagnosticsToolPrebuilt Drive FilesPrebuilt GCS
-LOCAL_OVERRIDES_PACKAGES += HelpRtcPrebuilt LocationHistoryPrebuilt MaestroPrebuilt Maps MarkupGoogle MicropaperPrebuilt MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += NgaResources OdadPrebuilt OemDmTrigger Photos PixelLiveWallpaperPrebuilt PrebuiltGmail RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SCONE SafetyHubPrebuilt ScribePrebuilt SecurityHubPrebuilt Showcase Snap SoundAmplifierPrebuilt SprintDM
-LOCAL_OVERRIDES_PACKAGES += SprintHM TurboAdapter TurboPrebuilt Tycho USCCDM VZWAPNLib VzwOmaTrigger WellbeingPrebuilt WfcActivation
-LOCAL_OVERRIDES_PACKAGES += arcore obdm_stub
+LOCAL_OVERRIDES_PACKAGES := \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	arcore \
+	BetterBug \
+	CalculatorGooglePrebuilt \
+	CalendarGoogle \
+	Camera2 \
+	CarrierMetrics \
+	CarrierSetup \
+	CarrierWifi \
+	CbrsNetworkMonitor \
+	ConnMO \
+	DCMO \
+	DevicePolicyPrebuilt \
+	DiagnosticsToolPrebuilt \
+	DMService \
+	Drive \
+	GCS \
+	GoogleCamera \
+	HelpRtcPrebuilt \
+	MaestroPrebuilt \
+	Maps \
+	MicropaperPrebuilt \
+	MyVerizonServices \
+	NgaResources \
+	OBDM_Permissions \
+	obdm_stub \
+	OdadPrebuilt \
+	OemDmTrigger \
+	Photos \
+	PrebuiltGmail \
+	RecorderPrebuilt \
+	SafetyHubPrebuilt \
+	SCONE \
+	ScribePrebuilt \
+	SecurityHubPrebuilt \
+	Showcase \
+	Snap \
+	SoundAmplifierPrebuilt \
+	SprintDM \
+	SprintHM \
+	talkback \
+	TetheringEntitlement \
+	TurboPrebuilt \
+	Tycho \
+	USCCDM \
+	VZWAPNLib \
+	VzwOmaTrigger \
+	WfcActivation
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
