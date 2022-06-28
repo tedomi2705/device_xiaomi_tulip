9eaff003bfb0c9abb7baabe0e84176d53d477fba
tedomi2705 <hongducthbk123@gmail.com>
2022-06-28 23:38:14 +0700

    Revert "twolip: Build libavservices_minijail"
    This reverts commit 60f6ae25c652202e6545aa5514f687f57326ba13.

--- device.mk
+++ device.mk
@@ -290,10 +290,6 @@ PRODUCT_PACKAGES += \
     android.hardware.lights-service.tulip
 
 # Media
-PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libavservices_minijail.vendor
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(DEVICE_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
